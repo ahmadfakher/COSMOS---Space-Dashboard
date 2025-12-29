253e4d7f1ee1c9d1552f3ef9e6a66cf8d4eb5217
Ahmad Fakher <ahmadfakhermohammed@gmail.com>
2025-12-30 00:28:56 +0200

    javascript changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,19 @@
 const APIkey = 'HsE4klHCKxFhJsvpec8wbdrB7F2ejkwOQvwdDeF3';
 const APOD_URL = `https://api.nasa.gov/planetary/apod?api_key=${APIkey}`;
 
-// NOTE: IMAGES ARE SOMETIMES VIDEOS, ASK ENG!!
+function error() {
+    const error = document.getElementById("apod-loading");
+    document.getElementById("apod-image").classList.add("hidden");
+    console.log(error);
+}
+error()
 
-function hideLoader(){
+function hideLoader() {
     document.querySelector("#apod-loading").classList.add("hidden");
     document.querySelector("#apod-image").classList.remove("hidden");
 }
 
-function showLoader(){
+function showLoader() {
     document.querySelector("#apod-loading").classList.remove("hidden");
     document.querySelector("#apod-image").classList.add("hidden");
     document.getElementById("apod-title").innerHTML = "Loading...";
@@ -43,7 +48,6 @@ async function getAPOD(date = "today") {
         // get data and display it
         const dataJson = await data.json();
         displayAPOD(dataJson);
-        console.log(dataJson);
     }
     catch (err) {
         console.log(err);
@@ -65,13 +69,30 @@ function displayAPOD(APOD) {
 
     // displaying image
     const img = document.getElementById('apod-image');
-    img.onload = () =>{
+    img.onload = () => {
         hideLoader();
     }
     img.onerror = () => {
         hideLoader();
     }
-    APOD.url ? img.src = APOD.url : img.src = "./assets/images/placeholder.webp";
+    let newElement;
+
+    if (APOD.media_type === "video") {
+        img.nextElementSibling.classList.add("hidden");
+        newElement = document.createElement("iframe");
+        newElement.src = APOD.url;
+        newElement.id = 'apod-image';
+        newElement.classList = 'w-full h-full object-cover hidden';
+        newElement.allowFullscreen = true;
+    } else {
+        img.nextElementSibling.classList.remove("hidden");
+        newElement = document.createElement("img");
+        newElement.classList = 'w-full h-full object-cover hidden';
+        newElement.id = 'apod-image';
+        newElement.src = APOD.url || "./assets/images/placeholder.webp";
+    }
+
+    img.replaceWith(newElement);
 
     // opening full res img
     APOD.hdurl ?
@@ -105,7 +126,7 @@ function displayAPOD(APOD) {
     if (APOD.copyright) {
         document.getElementById("apod-copyright").innerHTML = `© ${APOD.copyright}`;
         document.getElementById("apod-copyright").style.remove = "hidden";
-    } else { document.getElementById("apod-copyright").classList.add("hidden");}
+    } else { document.getElementById("apod-copyright").classList.add("hidden"); }
 
     // display media type
     document.getElementById("apod-media-type").innerHTML = APOD.media_type;
